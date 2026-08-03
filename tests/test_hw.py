92f0d2e2ffbf633a25c0c660b47eb5b65af5f71c
Henk Muller <henk@xmos.com>
2026-08-03 12:05:09 +0100

    Actually run the test

--- tests/test_hw.py
+++ tests/test_hw.py
@@ -25,7 +25,7 @@ def test_ro(request):
     with FileLock("xrun.lock"):
         run_cmd = f'xrun --id 0 --io --args {binary} {outfile}'
         print("Running cmd: ", run_cmd)
-#        stdout = subprocess.check_output(run_cmd, shell = True)
+        stdout = subprocess.check_output(run_cmd, shell = True)
 
     
     with open(outfile, "rb") as fd:
